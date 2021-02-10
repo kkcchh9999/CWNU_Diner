3454dce229c491e396b44d3f929d473bf53fa0de
kkcchh9999 <kkcchh9999@naver.com>
2021-02-10 14:35:46 +0900

    아 힘들었다 진짜로

--- app/src/main/java/com/example/cwnu_diner/SearchMenuActivity.java
+++ app/src/main/java/com/example/cwnu_diner/SearchMenuActivity.java
@@ -19,7 +19,7 @@ public class SearchMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_searchmenu);
 
         list = new ArrayList<>();
         settingList();
