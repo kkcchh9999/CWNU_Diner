3c83ba2b8c851168f58b3c3b4205c4c95b827699
da010228 <da010228@naver.com>
2021-02-11 17:15:12 +0900

    다은 수정

--- app/src/main/java/com/example/cwnu_diner/SearchStoreActivity.java
+++ app/src/main/java/com/example/cwnu_diner/SearchStoreActivity.java
@@ -7,6 +7,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -15,26 +16,32 @@ import java.util.List;
 
 public class SearchStoreActivity extends AppCompatActivity {
 
-    static final String[]List_Menu = {"List1", "List2", "List3"};
-    private ListView listView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchstore);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, List_Menu);
+        ListView listview;
+        ListViewAdapter adapter = new ListViewAdapter();
 
-        ListView listView = (ListView)findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(adapter);
 
-        /* 위젯과 멤버변수 참조 획득 */
-        //listView = (ListView)findViewById(R.id.listView);
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.a),
+                "a", "aaa");
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.b),
+                "b", "bbbbbbb");
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.c),
+                "c", "ccccc");
 
         /* 아이템 추가 및 어댑터 등록 */
         dataSetting();
 
-        ImageButton imageButton=findViewById(R.id.backBtn);
+        ImageButton imageButton = findViewById(R.id.backBtn);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
