e62468d5fadedcb327c33ff417108df495eb6b2e
sws256 <77598142+sws256@users.noreply.github.com>
2021-02-15 18:05:37 +0900

    서버연결클래스, 유저클래스

---
+++ app/src/main/java/com/example/cwnu_diner/User.java
@@ -0,0 +1,14 @@
+package com.example.cwnu_diner;
+
+public class User {
+    String userName;
+    String userID;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
