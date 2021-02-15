4651069e0fac01549a267be5a6673bc53eda694e
YoungEun-Choi930 <69569809+YoungEun-Choi930@users.noreply.github.com>
2021-02-15 17:18:35 +0900

    Add files via upload

---
+++ Server/userinsert.php
@@ -0,0 +1,42 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<?php
+
+$servername = 'localhost'; // 데이터베이스 호스트
+$username = 'root'; // 데이터베이스 ID (수정요망)
+$password = 'admin'; // 데이터베이스 PW (수정요망)
+$dbname = 'test'; //데이터베이스명 (수정요망)
+ 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if(!$conn) {
+    die("mysql connect failed");
+}
+
+// Select DB
+mysqli_select_db($conn, $dbname) or die('DB선택실패');
+
+// Form에서 넘어온 데이터
+$userName = $_POST['userName'];
+$userID = $_POST['userID'];
+
+// SQL Query 작성
+$sql = "INSERT INTO User (userName, userID)
+VALUES ('$userName', '$userID')";
+echo $sql;
+echo "<br>";
+
+// SQL Query 실행
+if(mysqli_query($conn, $sql)){
+    echo "Success!";
+}
+else{
+    echo "Error:".$query."mesage:".mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+// 요청이 끝난뒤 페이지를 이동
+header('Location:http://localhost/User.html');
+?>
