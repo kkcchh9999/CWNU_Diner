2b61ca1e346c11c501a7d5551a2d1a5433f66a29
da010228 <da010228@naver.com>
2021-02-15 17:02:05 +0900

    다은 저장... 머지 금지

--- app/src/main/java/com/example/cwnu_diner/SearchStoreActivity.java
+++ app/src/main/java/com/example/cwnu_diner/SearchStoreActivity.java
@@ -1,20 +1,29 @@
 
 package com.example.cwnu_diner;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
-import java.util.List;
 
 public class SearchStoreActivity extends AppCompatActivity {
+
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +58,41 @@ public class SearchStoreActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.toolbar_search);
+        searchView = (SearchView) menuItem.getActionView();
+        //searchView.setQueryHint(getResources().getString(R.string.query_hint));
+        searchView.setOnQueryTextListener(queryTextListener);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        if(null!= searchManager){
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        }
+        searchView.setIconifiedByDefault(true);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            // 텍스트 입력 후 검색 버튼이 눌렸을 때의 이벤트
+            TextView txt_text = (TextView)findViewById(R.id.txtresult);
+            String text = "에 대한 검색 결과 입니다.";
+            //txt_text.setText(query);
+            return true;
+        }
+
+        //텍스트가 바뀔때마다 호출
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            return false;
+        }
+    };
 
     private void dataSetting() {
 
