b23ee886701737f3572cb653f96aa32f2ae5e467
sws256 <77598142+sws256@users.noreply.github.com>
2021-02-15 18:13:51 +0900

    MainActivity.java, AndroidManifest.xml에 서버연결코드+ activity_main.xml 출력테스트코드 activity_storelist.xml 분류버튼만 붙여놓음

--- app/src/main/java/com/example/cwnu_diner/MainActivity.java
+++ app/src/main/java/com/example/cwnu_diner/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.cwnu_diner;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -20,10 +23,20 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button login;
+    private TextView tv_outPut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //위젯에 대한 참조.
+        tv_outPut = (TextView) findViewById(R.id.tv_outPut);
+        //URL 설정.
+        String url = "http://도메인명/try.php";
+
+        NetworkTask networkTask = new NetworkTask(url, null);
+        networkTask.execute();
+
 
         login = (Button)findViewById(R.id.btn_login);
         login.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +47,40 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
     }
+
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpConnection requestHttpConnection = new RequestHttpConnection();
+            result = requestHttpConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            tv_outPut.setText(s);
+        }
+        //[출처] 안드로이드+MYSQL+PHP 웹서버 연동하기 -2|작성자 흐이준
+    }
+
+
+
 }
