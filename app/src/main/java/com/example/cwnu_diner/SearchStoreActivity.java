dc5dc045846c17684dec2711e96ddb30057e5220
da010228 <da010228@naver.com>
2021-02-16 14:54:16 +0900

    다은 일단

--- app/src/main/java/com/example/cwnu_diner/SearchStoreActivity.java
+++ app/src/main/java/com/example/cwnu_diner/SearchStoreActivity.java
@@ -1,100 +1,74 @@
-
 package com.example.cwnu_diner;
 
-import android.app.SearchManager;
-import android.content.Context;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class SearchStoreActivity extends AppCompatActivity {
-
-    SearchView searchView;
+public class SearchStoreActivity extends Activity {
+    SearchAdapter adapter;
+    ListView listview = null;
+    ArrayAdapter<String> search_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchstore);
 
-        ListView listview;
-        ListViewAdapter adapter = new ListViewAdapter();
 
-        // 리스트뷰 참조 및 Adapter달기
-        listview = (ListView) findViewById(R.id.listView);
+        adapter = new SearchAdapter();
+
+        listview=(ListView)findViewById(R.id.search_listView);
         listview.setAdapter(adapter);
 
-        // 첫 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.a),
-                "a", "aaa");
-        // 두 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.b),
-                "b", "bbbbbbb");
-        // 세 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.c),
-                "c", "ccccc");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.b), "b", "bbbb");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.b), "bana", "bb");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.a), "apple", "aa");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.a), "app", "aaaaa");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.c), "cherry", "c");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.c), "cherrddy", "ccc");
 
-        /* 아이템 추가 및 어댑터 등록 */
-        dataSetting();
-
-        ImageButton imageButton = findViewById(R.id.backBtn);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                finish();
+            public void onItemClick(AdapterView parent, View v, int pos, long id) {
+                ListViewItem item = (ListViewItem)parent.getItemAtPosition(pos);
+
+                String titleStr = item.getName();
+                String descStr = item.getText();
+                Drawable iconDrawable = item.getIcon();
+
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_toolbar, menu);
+        EditText editTextFilter = (EditText)findViewById(R.id.editTextFilter) ;
+        editTextFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable edit) {
+            }
 
-        MenuItem menuItem = menu.findItem(R.id.toolbar_search);
-        searchView = (SearchView) menuItem.getActionView();
-        //searchView.setQueryHint(getResources().getString(R.string.query_hint));
-        searchView.setOnQueryTextListener(queryTextListener);
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-        if(null!= searchManager){
-            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        }
-        searchView.setIconifiedByDefault(true);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    private SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
-        @Override
-        public boolean onQueryTextSubmit(String query) {
-            // 텍스트 입력 후 검색 버튼이 눌렸을 때의 이벤트
-            TextView txt_text = (TextView)findViewById(R.id.txtresult);
-            String text = "에 대한 검색 결과 입니다.";
-            //txt_text.setText(query);
-            return true;
-        }
-
-        //텍스트가 바뀔때마다 호출
-        @Override
-        public boolean onQueryTextChange(String newText) {
-            return false;
-        }
-    };
-
-    private void dataSetting() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            SearchStoreActivity.this.search_adapter.getFilter().filter(s);
+            }
+        });
 
     }
 
