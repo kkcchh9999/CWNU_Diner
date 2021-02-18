f1a66baa83f0a81210524bb47e4af58d72def490
YoungEun-Choi930 <69569809+YoungEun-Choi930@users.noreply.github.com>
2021-02-18 17:42:47 +0900

    json파일 추가

---
+++ Server/menuData.php
@@ -0,0 +1,20 @@
+<?php
+    $db = mysqli_connect('localhost','root','admin','test');
+    if(!$db) { die("fail");   }
+    $query = "select * from Menu";
+    $result = mysqli_query($db,$query);
+
+    $resultArray = array();
+    while($row = mysqli_fetch_array($result)){
+        $insertArray = array();
+        $insertArray['menu'] = $row[menu];
+        $insertArray['storeName'] = $row[storeName];
+        $insertArray['price'] = $row[price];
+        $insertArray['type'] = $row[type];
+        
+        array_push($resultArray, $insertArray);
+        }
+    $output = json_encode($resultArray,JSON_UNESCAPED_UNICODE);
+    echo urldecode($output);
+    
+?>
