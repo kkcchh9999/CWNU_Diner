6eb5bb4306e39851647a720a066a30a7d25b492f
kkcchh9999 <kkcchh9999@naver.com>
2021-02-25 17:20:47 +0900

    다시

--- app/src/main/java/com/example/cwnu_diner/AddReviewActivity.java
+++ app/src/main/java/com/example/cwnu_diner/AddReviewActivity.java
@@ -1,14 +1,48 @@
 package com.example.cwnu_diner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class AddReviewActivity extends AppCompatActivity {
+
+    Button btn_reviewAdd;
+    EditText et_review;
+    RatingBar ratingBar;
+    StoreData data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_review);
+
+        /////툴바 달기
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ///인텐트로 정보 받아오기
+        Intent intent = getIntent();
+        if(intent.getExtras() != null){
+            data = (StoreData) intent.getSerializableExtra("data");
+        }
+
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+
+        btn_reviewAdd = (Button)findViewById(R.id.btn_reviewAdd);
+        btn_reviewAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/cwnu_diner/ReviewData.java
+++ app/src/main/java/com/example/cwnu_diner/ReviewData.java
@@ -2,62 +2,37 @@ package com.example.cwnu_diner;
 
 public class ReviewData {
 
-    String storeName;
-    String menu;
-    String userID;
-    String review;
-    String date;
-    int starRating;
+    String userID, storeName, review, menu;
+    String starRating;
+
+    public String getUserID() {
+        return userID;
+    }
 
     public String getStoreName() {
         return storeName;
     }
 
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
+    public String getReview() {
+        return review;
     }
 
     public String getMenu() {
         return menu;
     }
 
-    public void setMenu(String menu) {
-        this.menu=menu;
-    }
-
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
-
-    }
-
-    public String getReview() {
-        return review;
-    }
-    public void setReview(String review) {
-        this.review = review;
-    }
-
-    public String getDate(){ return date; }
-
-    public void setDate(String date) { this.date = date; }
-
-    public int getStarRating() {
+    public String getStarRating() {
         return starRating;
     }
 
-    public void setStarRating(int starRating) { this.starRating = starRating; }
-
-    public ReviewData(String storeName, String menu, String userID, String review, String date, int starRating) {
-        this.storeName=storeName;
-        this.menu=menu;
-        this.userID=userID;
-        this.review=review;
-        this.date=date;
-        this.starRating=starRating;
+    public ReviewData(String userID, String storeName, String review, String menu, String starRating)
+    {
+        this.userID = userID;
+        this.storeName = storeName;
+        this.review = review;
+        this.menu = menu;
+        this.starRating = starRating;
     }
+
+
 }
