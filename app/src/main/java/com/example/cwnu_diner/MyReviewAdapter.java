07215011e75dbcb4df037a51d18eef14bcb4a0c6
dmswls4634 <69571885+dmswls4634@users.noreply.github.com>
2021-02-25 17:48:05 +0900

    은진 수정

---
+++ app/src/main/java/com/example/cwnu_diner/MyReviewAdapter.java
@@ -0,0 +1,64 @@
+package com.example.cwnu_diner;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyReviewAdapter extends RecyclerView.Adapter <MyReviewAdapter.ItemViewHolder>{
+    ArrayList<MyReviewData> myReviewList;
+
+    public MyReviewAdapter(ArrayList<MyReviewData> myReviewList){
+        this.myReviewList=myReviewList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return myReviewList.size();
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemview = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_item, parent, false);
+        return new ItemViewHolder(itemview);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ItemViewHolder holder,final int position) {
+        MyReviewData review=myReviewList.get(position);
+        String starRating=Integer.toString(review.getStarRating());
+
+        holder.userID.setText(review.getUserID());
+        holder.storeName.setText(review.getStoreName());
+        holder.review.setText(review.getReview());
+        holder.menu.setText(review.getMenu());
+        holder.starRating.setText(starRating);
+        holder.date.setText(review.getDate());
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder{
+        TextView storeName;
+        TextView menu;
+        TextView userID;
+        TextView review;
+        TextView date;
+        TextView starRating;
+
+        ItemViewHolder(@NonNull View ReviewView) {
+            super(ReviewView);
+
+            storeName=ReviewView.findViewById(R.id.txt_storeName);
+            menu=ReviewView.findViewById(R.id.txt_menu);
+            userID=ReviewView.findViewById(R.id.txt_userID);
+            review=ReviewView.findViewById(R.id.txt_review);
+            date=ReviewView.findViewById(R.id.txt_date);
+            starRating=ReviewView.findViewById(R.id.txt_starRating);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cwnu_diner/MyReviewData.java
@@ -0,0 +1,43 @@
+package com.example.cwnu_diner;
+
+public class MyReviewData {
+    String storeName;
+    String menu;
+    String userID;
+    String review;
+    String date;
+    int starRating;
+
+    public String getStoreName() { return storeName; }
+
+    public void setStoreName(String storeName) { this.storeName = storeName; }
+
+    public String getMenu() { return menu; }
+
+    public void setMenu(String menu) { this.menu=menu; }
+
+    public String getUserID() { return userID; }
+
+    public void setUserID(String userID) { this.userID = userID; }
+
+    public String getReview() { return review; }
+
+    public void setReview(String review) { this.review = review; }
+
+    public String getDate(){ return date; }
+
+    public void setDate(String date) { this.date = date; }
+
+    public int getStarRating() { return starRating; }
+
+    public void setStarRating(int starRating) { this.starRating = starRating; }
+
+    public MyReviewData(String storeName, String menu, String userID, String review, String date, int starRating) {
+        this.storeName=storeName;
+        this.menu=menu;
+        this.userID=userID;
+        this.review=review;
+        this.date=date;
+        this.starRating=starRating;
+    }
+}
