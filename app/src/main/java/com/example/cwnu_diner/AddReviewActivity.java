9cb4230157e69053db2ccd8d4fed5ada406f6970
da010228 <da010228@naver.com>
2021-02-26 15:30:43 +0900

    다은

--- app/src/main/java/com/example/cwnu_diner/AddReviewActivity.java
+++ app/src/main/java/com/example/cwnu_diner/AddReviewActivity.java
@@ -29,6 +29,7 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -61,14 +62,18 @@ public class AddReviewActivity extends AppCompatActivity {
 
         ///인텐트로 정보 받아오기
         Intent intent = getIntent();
-        if(intent.getExtras() != null){
+        if (intent.getExtras() != null) {
             data = (StoreData) intent.getSerializableExtra("data");
             userID = intent.getStringExtra("userID");
         }
 
+        ////
+        final TextView store = findViewById(R.id.store);
+        store.setText(data.getStoreName());
+
         //////////////메뉴 데이터 받아오기
         String serverURL = "http://3.34.134.116/menuData.php";
-        JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(Request.Method.POST, serverURL, null, new Response.Listener<JSONArray>() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, serverURL, null, new Response.Listener<JSONArray>() {
             //volley 라이브러리의 GET방식은 버튼 누를때마다 새로운 갱신 데이터를 불러들이지 않음. 그래서 POST 방식 사용
             @Override
             public void onResponse(JSONArray response) {
@@ -76,20 +81,22 @@ public class AddReviewActivity extends AppCompatActivity {
                 //파라미터로 응답받은 결과 JsonArray를 분석
                 menuData.clear();
                 try {
-                    for(int i=0;i<response.length();i++){
-                        JSONObject jsonObject= response.getJSONObject(i);
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject jsonObject = response.getJSONObject(i);
+
+                        String storeName = jsonObject.getString("storeName");
+                        if (storeName.equals(data.getStoreName())) {
 
 
                         String menu = jsonObject.getString("menu");
-                        String storeName=jsonObject.getString("storeName");
+                        //String storeName=jsonObject.getString("storeName");
                         int price = jsonObject.getInt("price");
-                        String menuType=jsonObject.getString("menuType");
+                        String menuType = jsonObject.getString("menuType");
 
+                        menuData.add(new MenuData(menu, storeName, price, menuType));
+                        arrayList.add(menu);
+                        arrayAdapter.notifyDataSetChanged();
 
-                        if(storeName.equals(data.getStoreName())) {
-                            menuData.add(new MenuData(menu, storeName, price, menuType));
-                            arrayList.add(menu);
-                            arrayAdapter.notifyDataSetChanged();
                         }
                     }
 
@@ -103,17 +110,17 @@ public class AddReviewActivity extends AppCompatActivity {
             }
         });
         //실제 요청 작업을 수행해주는 요청큐 객체 생성
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
         //요청큐에 요청 객체 생성
         requestQueue.add(jsonArrayRequest);
-        spinner = (Spinner)findViewById(R.id.spinner_menu);
-        arrayAdapter = new ArrayAdapter<>(this , android.R.layout.simple_spinner_dropdown_item,arrayList);
+        spinner = (Spinner) findViewById(R.id.spinner_menu);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, arrayList);
         spinner.setAdapter(arrayAdapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                ((TextView)view).setText(arrayList.get(i));
-                Toast.makeText(getApplicationContext(),arrayList.get(i),Toast.LENGTH_SHORT).show();
+                ((TextView) view).setText(arrayList.get(i));
+                //Toast.makeText(getApplicationContext(),arrayList.get(i),Toast.LENGTH_SHORT).show();
                 menu = arrayList.get(i);
             }
 
@@ -123,17 +130,17 @@ public class AddReviewActivity extends AppCompatActivity {
             }
         });
 
-        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                rating = (int)v;
+                rating = (int) v;
             }
         });
 
-        et_review = (EditText)findViewById(R.id.et_review);
+        et_review = (EditText) findViewById(R.id.et_review);
 
-        btn_reviewAdd = (Button)findViewById(R.id.btn_reviewAdd);
+        btn_reviewAdd = (Button) findViewById(R.id.btn_reviewAdd);
         btn_reviewAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -152,15 +159,13 @@ public class AddReviewActivity extends AppCompatActivity {
     }
 
 
-
-    class InsertData extends AsyncTask<String, Void, String >
-    {
+    class InsertData extends AsyncTask<String, Void, String> {
         ProgressDialog progressDialog;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            progressDialog = ProgressDialog.show(AddReviewActivity.this,"Please Wait",null, true, true);
+            progressDialog = ProgressDialog.show(AddReviewActivity.this, "Please Wait", null, true, true);
         }
 
         @Override
@@ -173,16 +178,16 @@ public class AddReviewActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 
-            String userID = (String)params[1];
-            String store = (String)params[2];
-            String menu = (String)params[4];
-            String review = (String)params[3];
+            String userID = (String) params[1];
+            String store = (String) params[2];
+            String menu = (String) params[4];
+            String review = (String) params[3];
             int rating = (Integer.parseInt(params[5]));
-            String serverURL = (String)params[0];
+            String serverURL = (String) params[0];
 
-            String postParameters = "userID="+userID+"&storeName="+store+"&review="+review+"&menu="+menu+"&starRating="+rating;
+            String postParameters = "userID=" + userID + "&storeName=" + store + "&review=" + review + "&menu=" + menu + "&starRating=" + rating;
 
-            try{
+            try {
                 URL url = new URL(serverURL);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
@@ -193,16 +198,15 @@ public class AddReviewActivity extends AppCompatActivity {
 
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 outputStream.write(postParameters.getBytes("UTF-8"));
-                Log.d("tagggggg",postParameters);
+                Log.d("tagggggg", postParameters);
                 outputStream.flush();
                 outputStream.close();
 
 
-
                 int responseStatusCode = httpURLConnection.getResponseCode();
-                Log.d("TAGGGGGG","POSTrsponsecode"+responseStatusCode);
+                Log.d("TAGGGGGG", "POSTrsponsecode" + responseStatusCode);
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 Log.d("TAGGGWRONG", "아진짜 이게 터지네");
             }
 
