f505e3ed83ff07db341a41a3fec6000507508f6f
da010228 <da010228@naver.com>
2021-02-27 13:53:02 +0900

    검색 버튼 클릭

--- app/src/main/java/com/example/cwnu_diner/SplashActivity.java
+++ app/src/main/java/com/example/cwnu_diner/SplashActivity.java
@@ -20,13 +20,13 @@ public class SplashActivity extends Activity {
 
         //야간모드 유지
         AppData=getSharedPreferences("AppData", Activity.MODE_PRIVATE);
-        Boolean switch_state=AppData.getBoolean("switch_state",false);
-        if(switch_state==true){
+        boolean switch_state=AppData.getBoolean("switch_state",false);
+        if(switch_state){
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            findViewById(R.id.splash).setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.color_dark_000000_ffffff));
         }
         else{
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            findViewById(R.id.splash).setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.color_dark_000000_ffffff));
         }
 
         Handler handler = new Handler();
